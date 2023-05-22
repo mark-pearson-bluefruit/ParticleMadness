7849a68bb0366a03d5481076305e640fddfd8184
Mark Pearson <mark.pearson@bluefruit.co.uk>
2023-05-22 17:29:56 +0100

    Code for simple 2D collision

--- src/collisioncalculation.c
+++ src/collisioncalculation.c
@@ -3,3 +3,7 @@
 float velocityAfterOneDimCollision(float mass1, float mass2, float velocityBefore1, float velocityBefore2) {
     return ((mass1 - mass2)*velocityBefore1 + 2.*mass2*velocityBefore2)/(mass1 + mass2);
 }
+
+Vector2 velocityAfterTwoDimCollision(float mass1, float mass2, Vector2 velocityBefore1, Vector2 velocityBefore2, Vector2 unitNormal) {
+    return {velocityAfterOneDimCollision(mass1, mass2, velocityBefore1.x, velocityBefore2.x), 0.0f};
+}
--- src/include/collisioncalculation.h
+++ src/include/collisioncalculation.h
@@ -1,6 +1,9 @@
 #ifndef COLLISIONCALCULATION_H
 #define COLLISIONCALCULATION_H
 
+#include "Raylib.h"
+
 float velocityAfterOneDimCollision(float mass1, float mass2, float velocityBefore1, float velocityBefore2); // PRIVATE FUNCTION
+Vector2 velocityAfterTwoDimCollision(float mass1, float mass2, Vector2 velocityBefore1, Vector2 velocityBefore2, Vector2 unitNormal);
 
 #endif // COLLISIONCALCULATION_H
