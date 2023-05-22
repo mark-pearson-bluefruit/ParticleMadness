448e02275e438893081ff660942ad4c23b514380
Mark Pearson <mark.pearson@bluefruit.co.uk>
2023-05-22 15:51:43 +0100

    Implement code for complex one D collisions

--- src/collisioncalculation.c
+++ src/collisioncalculation.c
@@ -1,5 +1,5 @@
 #include "collisioncalculation.h"
 
 float velocityAfterOneDimCollision(float mass1, float mass2, float velocityBefore1, float velocityBefore2) {
-    return velocityBefore2;
+    return ((mass1 - mass2)*velocityBefore1 + 2.*mass2*velocityBefore2)/(mass1 + mass2);
 }
