dfe99a8c3104029f2a726a2317901a61249f890a
Mark Pearson <mark.pearson@bluefruit.co.uk>
2023-05-23 16:41:28 +0100

    example of many particle collisions

--- src/main.c
+++ src/main.c
@@ -2,6 +2,66 @@
 #include "nextstep.h"
 #include "render.h"
 
+enum {
+    MAX_NUMBER_OF_PARTICLES = 2000,
+    screenWidth = 800,
+    screenHeight = 400,
+};
+
+// Examples
+void example1(Rectangle* window, Vector2* resolution, size_t* numberOfParticles, particle* particles) {
+    *numberOfParticles = 2;
+    window->x = 0.;
+    window->y = 10.;
+    window->height = 10;
+    window->width = 20;
+
+    resolution->x = screenWidth;
+    resolution->y = screenHeight;
+
+    particles[0].position.x = 5.;
+    particles[0].position.y = 5.;
+    particles[0].velocity.x = 5.; 
+    particles[0].velocity.y = 4.0;
+    particles[0].radius = 1.;
+    particles[0].color = MAROON;
+    particles[0].mass = 1.;
+
+    particles[1].position.x = 7.;
+    particles[1].position.y = 7.;
+    particles[1].velocity.x = 5.; 
+    particles[1].velocity.y = 8.0;
+    particles[1].radius = 0.5;
+    particles[1].color = YELLOW;
+    particles[1].mass = 0.25;
+}
+
+// Examples
+void example2(Rectangle* window, Vector2* resolution, size_t* numberOfParticles, particle* particles) {
+    *numberOfParticles = 250;
+    window->x = 0.;
+    window->y = 50.;
+    window->height = 50;
+    window->width = 100;
+
+    resolution->x = screenWidth;
+    resolution->y = screenHeight;
+
+
+    for (size_t i = 0; i < *numberOfParticles; i++) {
+        particles[i].position.x = (float)((2*i+1)%100);
+        particles[i].position.y = (float) (2 + (2*i+1)/100);
+        particles[i].velocity.x = 3.;
+        particles[i].velocity.y = 35.;
+        particles[i].radius = 0.5;
+        particles[i].color = MAROON;
+        particles[i].mass = 1.;
+    }
+    particles[0].color = WHITE;
+    particles[0].velocity.y = -3.;
+    particles[0].velocity.x = 3.;
+}
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -15,31 +75,15 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    const float deltaTime = 1/240.;
 
-    const float deltaTime = 1./60.;
-    const size_t numberOfParticles = 2;
-    particle particles[numberOfParticles];
     Rectangle window;
-    window.x = 0.;
-    window.y = 10.;
-    window.height = 10;
-    window.width = 20;
+    Vector2 resolution;
+    size_t numberOfParticles;
+    particle particles[MAX_NUMBER_OF_PARTICLES];
 
-    Vector2 resolution = {screenWidth, screenHeight};
+    example2(&window, &resolution, &numberOfParticles, particles);
 
-    particles[0].position.x = 5.;
-    particles[0].position.y = 5.;
-    particles[0].velocity.x = 65.; 
-    particles[0].velocity.y = 80.0;
-    particles[0].radius = 1.;
-    particles[0].color = MAROON;
-
-    particles[1].position.x = 7.;
-    particles[1].position.y = 7.;
-    particles[1].velocity.x = 5.; 
-    particles[1].velocity.y = 8.0;
-    particles[1].radius = 0.5;
-    particles[1].color = YELLOW;
 
     //--------------------------------------------------------------------------------------
     // Main game loop
@@ -50,6 +94,9 @@ int main(void)
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
         nextStep(particles, numberOfParticles, deltaTime, window);
+        nextStep(particles, numberOfParticles, deltaTime, window);
+        nextStep(particles, numberOfParticles, deltaTime, window);
+        nextStep(particles, numberOfParticles, deltaTime, window);
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
@@ -57,7 +104,6 @@ int main(void)
         ClearBackground(BLACK);
 
         displayParticle(particles, numberOfParticles, window, resolution);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
