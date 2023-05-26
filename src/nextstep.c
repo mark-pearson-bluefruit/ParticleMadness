e5166ba5c9b55bbcc88a592588c530754c0022d4
Mark Pearson <mark.pearson@bluefruit.co.uk>
2023-05-26 11:16:44 +0100

    implementation of particle to bounding wall collision

--- src/nextstep.c
+++ src/nextstep.c
@@ -23,16 +23,20 @@ void nextStep(particle* particles, size_t number_of_particles, float deltaTime, 
         // Detect and correct Collision
 
         // Bounding Box Detection And Correction
+        /*
         if (particles[i].position.x + particles[i].radius > box.x + box.width || particles[i].position.x - particles[i].radius < box.x) {
             particles[i].velocity.x = -particles[i].velocity.x;
         }
         if (particles[i].position.y + particles[i].radius > box.y || particles[i].position.y - particles[i].radius < box.y - box.height) {
             particles[i].velocity.y = -particles[i].velocity.y;
         }
+        */
 
         // Add velocity to position
         particles[i].position.x += deltaTime*particles[i].velocity.x;
         particles[i].position.y += deltaTime*particles[i].velocity.y;
+
+        positionVelocityCorrectionWithBoundingBox(&particles[i], box);
 
     }
 }
