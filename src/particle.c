b57106878d0e791976dedf178696c7ea97d64d0b
Mark Pearson <mark.pearson@bluefruit.co.uk>
2023-05-30 14:16:26 +0100

    change so only returns true when particles overlap not touch

--- src/particle.c
+++ src/particle.c
@@ -5,5 +5,5 @@ bool doParticlesOverlap(particle particle1, particle particle2) {
     Vector2 direction = Vector2Subtract(particle1.position,  particle2.position);
     float distanceSquared = Vector2DotProduct(direction, direction);
     float radiusSquared = (particle1.radius + particle2.radius)*(particle1.radius + particle2.radius);
-    return (distanceSquared <= radiusSquared);
+    return (distanceSquared < radiusSquared);
 }
