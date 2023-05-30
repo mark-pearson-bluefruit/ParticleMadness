ac0419c353d0354031ffcec713e5171a0219b97a
Mark Pearson <mark.pearson@bluefruit.co.uk>
2023-05-30 14:15:33 +0100

    Debug for build

--- build_app.bat
+++ build_app.bat
@@ -1,5 +1,5 @@
 if not exist .\build (
     mkdir build
 )
-gcc src/main.c src/nextstep.c src/render.c src/collisioncalculation.c src/particle.c -o build/particleMadness.exe -O1 -Wall -std=c99 -Wno-missing-braces -I src/include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc src/main.c src/nextstep.c src/render.c src/collisioncalculation.c src/particle.c -o build/particleMadness.exe -O1 -Wall -std=c99 -Wno-missing-braces -I src/include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -g
 build\particleMadness.exe
